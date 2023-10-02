904ea096bc842f37c847aa6caa6b11c5c6c55c65
“Salvo1789” <salvo.mercurio89@gmail.com>
2023-10-02 10:54:35 +0200

    Created cards data and Card component

---
+++ mememory/src/components/Card.jsx
@@ -0,0 +1,17 @@
+
+
+function Card({card, onClick}) {
+    return(
+        <div 
+        className={`memory-card${card.isFlipped ? " flip" : ""}`}
+        onClick={onClick}
+        data-testid={card.id}
+        >
+        <img className="front-face" src={card.image} alt="Card" />
+        <img className="back-face" src="{https://img.wallpapic.it/i4926-333-617/thumb/blu-colorati-arte-digitale-sfondo.jpg" alt="JS Badge" />
+
+        </div>
+    )
+}
+
+export default Card;
